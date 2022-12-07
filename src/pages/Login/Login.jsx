b935610b3925551361dca73e0e17ae46022754f9
surajgaire14 <eriag321@gmail.com>
2022-12-08 01:09:09 +0545

    login page added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "./styles/style.css";
+import metamask from "../../images/metamask.png";
 
 const Login = () => {
   return (
     <div className="login__container">
       <div className="login">
-        <button>Login with Metamask</button>
+        <div className="button__container">
+          <button>Login with Metamask</button>
+          <img src={metamask} alt="metamask" />
+        </div>
       </div>
     </div>
   );
