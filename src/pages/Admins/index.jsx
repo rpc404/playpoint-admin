4498fd2f64cdd71e47b03157143184c00fedf3bf
surajgaire14 <eriag321@gmail.com>
2022-12-26 08:20:24 +0545

    admin page updated

--- src/pages/Admins/index.jsx
+++ src/pages/Admins/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { toast } from "react-toastify";
 import { addAdmin, allAdmins, removeAdmin } from "../../api/AdminStats";
-import "./styles/style.css"
+import "./styles/style.css";
 import {
   TextField,
   Select,
@@ -90,86 +90,97 @@ const Admin = () => {
     }
   };
   return (
-    <Box className="admin__container">
-      <Tabs
-        orientation="vertical"
-        value={value}
-        onChange={handleChange}
-        aria-label="Vertical tabs example"
-        sx={{ borderRight: 1, borderColor: "divider" }}
-      >
-        <Tab label="Add Admin" {...a11yProps(0)} />
-        <Tab label="Admins" {...a11yProps(1)} />
-      </Tabs>
-      <TabPanel value={value} index={0}>
-        <div className="form__container">
-          <div className="form">
-            <h3>Add admin</h3>
-            <TextField
-              id="name"
-              variant="outlined"
-              label="name"
-              value={name}
-              onChange={(e) => setname(e.target.value)}
-            />
-            <TextField
-              label="address"
-              onChange={(e) => setWallet(e.target.value)}
-            />
-            <FormControl>
-              <InputLabel id="demo-simple-select-label">Choose Role</InputLabel>
-              <Select
-                onChange={(e) => setRole(e.target.value)}
-                value={role}
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                label="Choose Role"
-              >
-                <MenuItem value="admin">Admin</MenuItem>
-                <MenuItem value="superadmin">Super Admin</MenuItem>
-                <MenuItem value="analytics">Analytics</MenuItem>
-              </Select>
-            </FormControl>
-            <button onClick={() => handleSubmit()}>add admin</button>
+    <div className="container">
+      <div className="admin_nav">
+        <h3>Playpoint</h3>
+        <img
+          src="https://ik.imagekit.io/domsan/Logo_0vBSw9piY.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1662803005580"
+          alt="playpoint_logo"
+        />
+      </div>
+      <Box className="admin__container">
+        <Tabs
+          orientation="vertical"
+          value={value}
+          onChange={handleChange}
+          aria-label="Vertical tabs example"
+          sx={{ borderRight: 1, borderColor: "divider" }}
+        >
+          <Tab label="Add Admin" {...a11yProps(0)} />
+          <Tab label="Admins" {...a11yProps(1)} />
+        </Tabs>
+        <TabPanel value={value} index={0}>
+          <div className="form__container">
+            <div className="form">
+              <h3>Add admin</h3>
+              <TextField
+                id="name"
+                variant="outlined"
+                label="name"
+                value={name}
+                onChange={(e) => setname(e.target.value)}
+              />
+              <TextField
+                label="address"
+                onChange={(e) => setWallet(e.target.value)}
+              />
+              <FormControl>
+                <InputLabel id="demo-simple-select-label">
+                  Choose Role
+                </InputLabel>
+                <Select
+                  onChange={(e) => setRole(e.target.value)}
+                  value={role}
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  label="Choose Role"
+                >
+                  <MenuItem value="admin">Admin</MenuItem>
+                  <MenuItem value="superadmin">Super Admin</MenuItem>
+                  <MenuItem value="analytics">Analytics</MenuItem>
+                </Select>
+              </FormControl>
+              <button onClick={() => handleSubmit()}>add admin</button>
+            </div>
           </div>
-        </div>
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        <div className="all_admins">
-          <h2>Admins</h2>
-          <div className="admins">
-            {admins.map((admin, index) => {
-              return (
-                <div className="box" key={index}>
-                  <p>Name : {admin.name}</p>
-                  <p>
-                    <i className="ri-wallet-line"></i> {admin.wallet}{" "}
-                    <i
-                      className="ri-clipboard-line"
-                      title="copy to clipboard"
-                      onClick={() =>
-                        window.navigator.clipboard
-                          .writeText(admin.wallet)
-                          .then(() => {
-                            toast("wallet address copied!");
-                          })
-                      }
-                    ></i>
-                  </p>
-                  <p>Role : {admin.role}</p>
-                  <div className="button__container">
-                    <button onClick={() => deleteAdmin(admin._id)}>
-                      Remove
-                    </button>
-                    <i className="ri-delete-bin-line"></i>
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          <div className="all_admins">
+            <h2>Admins</h2>
+            <div className="admins">
+              {admins.map((admin, index) => {
+                return (
+                  <div className="box" key={index}>
+                    <p>Name : {admin.name}</p>
+                    <p>
+                      <i className="ri-wallet-line"></i> {admin.wallet}
+                      <i
+                        className="ri-clipboard-line"
+                        title="copy to clipboard"
+                        onClick={() =>
+                          window.navigator.clipboard
+                            .writeText(admin.wallet)
+                            .then(() => {
+                              toast("wallet address copied!");
+                            })
+                        }
+                      ></i>
+                    </p>
+                    <p>Role : {admin.role}</p>
+                    <div className="button__container">
+                      <button onClick={() => deleteAdmin(admin._id)}>
+                        Remove
+                      </button>
+                      <i className="ri-delete-bin-line"></i>
+                    </div>
                   </div>
-                </div>
-              );
-            })}
+                );
+              })}
+            </div>
           </div>
-        </div>
-      </TabPanel>
-    </Box>
+        </TabPanel>
+      </Box>
+    </div>
   );
 };
 
